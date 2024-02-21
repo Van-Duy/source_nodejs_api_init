51d134c3d213715819875a31dca06725787719ab
VanDuy <vanduy@gmail.com>
2024-02-21 16:03:21 +0700

    init source

---
+++ src/v1/routes/index.router.js
@@ -0,0 +1,14 @@
+"use strict"
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Api ok'
+    })
+})
+
+
+module.exports = router;
